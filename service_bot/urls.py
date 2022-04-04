9a445355dfaae5d6645c4ea41ac92953d590c5f1
vadym <vadim.melnichenko@gmail.com>
2022-04-04 10:03:11 +0100

    del some files, add postgresql, add admin=anaconda

--- service_bot/urls.py
+++ service_bot/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('anaconda/', admin.site.urls),
     path('api/', include('beauty.urls')),
     path('', include('website.urls')),
 ]
